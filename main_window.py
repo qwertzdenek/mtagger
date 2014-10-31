d439ad5c1373283eadf81939ebb35d6377d27045
qwertzdenek <jan.zdenek@gmail.com>
2014-10-31 17:08:29 +0100

    open file dialog and filelist

--- main_window.py
+++ main_window.py
@@ -1,5 +1,4 @@
 from gi.repository import Gtk
-import wave
 
 class MainWindow:
     def __init__(self):
@@ -8,20 +7,23 @@ class MainWindow:
         self.builder.connect_signals(self)
         
         self.window = self.builder.get_object("window")
+        self.file_dialog = self.builder.get_object("file_dialog")
+        self.file_store = self.builder.get_object("filestore")
+        
         self.window.show_all()
 
     def open_button_clicked_cb(self, button):
-        dialog = Gtk.FileChooserDialog("Zvolte soubor", self,
-            Gtk.FileChooserAction.OPEN,
-            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-             Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+		# TODO: set directory from the self.last_dir
+        response = self.file_dialog.run()
         
-        response = dialog.run()
-        #if response == Gtk.ResponseType.OK:
-        #    self.snd = wave.open(dialog.get_filename(), 'rb')
-        #    print(self.snd);
+        if response == 1:
+            self.last_dir = self.file_dialog.get_path()
+            for f in self.file_dialog.get_filenames():
+                self.file_store.append([f, "N/A"])
+        elif response == 0:
+            print("response 0")
 
-        dialog.destroy()
+        self.file_dialog.hide()
 
     def del_button_clicked_cb(self, button):
         print("del_button_clicked_cb")
@@ -29,10 +31,10 @@ class MainWindow:
         print("play_button_clicked_cb")
     def cl_button_clicked_cb(self, button):
         print("cl_button_clicked_cb")
+        # TODO: send signal to the classificator
 
     def destroy(self, window):
         Gtk.main_quit()
 
     def main(self):
         Gtk.main()
-
