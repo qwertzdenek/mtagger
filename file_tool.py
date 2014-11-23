7a7ae8c29b99c4850efb6616e69d777e184dced1
Zdeněk Janeček <jan.zdenek@gmail.com>
2014-11-23 01:01:23 +0100

    copyright notices

--- file_tool.py
+++ file_tool.py
@@ -1,3 +1,17 @@
+"""
+Copyright (c) 2014 Zdeněk Janeček
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+"""
+
 import gi, numpy as np
 gi.require_version('Gst', '1.0')
 from gi.repository import GLib, Gst
--- main_window.py
+++ main_window.py
@@ -1,3 +1,17 @@
+"""
+Copyright (c) 2014 Zdeněk Janeček
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+"""
+
 import pickle
 import numpy as np
 from gi.repository import Gtk
@@ -108,6 +122,7 @@ class MainWindow:
         if class_id == -1:
             return
         self.fill(class_id)
+        self.classcombo.get_child().set_text("")
 
     # vytvoří třídu
     def classcombo_entry_activated_cb(self, entry):
