fb7992aa78386a99d14d1d7647f44b1cc6b88ce5
qwertzdenek <jan.zdenek@gmail.com>
2015-01-03 18:18:26 +0100

    data range modification

--- stats.py
+++ stats.py
@@ -50,53 +50,55 @@ instruments = os.listdir()
 X = []
 y = []
 
-take=20
-clsid=0
-for inst in instruments:
-    names = os.listdir(path=inst)
-    names = random.sample(names, take)
-    for f in names:
-        samples = open_file(os.path.join(os.getcwd(), inst, f), 16000)
-        feat = mfcc(samples,16000, appendEnergy=False, winlen=0.030, VAD=simpleVAD)
-        # add two symptoms from the middle
-        X.append(feat[len(feat) / 2 + 5])
-        y.append(clsid)
-        X.append(feat[len(feat) / 2 - 3])
-        y.append(clsid)
-        X.append(feat[len(feat) / 2 - 5])
-        y.append(clsid)
-    clsid += 1
-    
-#clf = svm.SVC()
+for take in range(5, 25):
+    clsid=0
+    set_size = 0
+    for inst in instruments:
+        names = os.listdir(path=inst)
+        names = random.sample(names, take)
+        for f in names:
+            samples = open_file(os.path.join(os.getcwd(), inst, f), 16000)
+            feat = mfcc(samples,16000, appendEnergy=False, winlen=0.030, VAD=simpleVAD)
+            # add two symptoms from the middle
+            X.append(feat[len(feat) / 2 + 5])
+            y.append(clsid)
+            X.append(feat[len(feat) / 2 - 3])
+            y.append(clsid)
+            X.append(feat[len(feat) / 2 - 5])
+            y.append(clsid)
+            set_size += 3
+        clsid += 1
+        
+    clf = svm.SVC()
 
-#param_grid = {'C': [0.5, 5, 50, 500], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']}
+    param_grid = {'C': [0.5, 5, 50, 500], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']}
 
-#gse = grid_search.GridSearchCV(clf, param_grid, n_jobs=os.cpu_count())
-#gse.fit(X, y)
+    gse = grid_search.GridSearchCV(clf, param_grid, n_jobs=os.cpu_count())
+    gse.fit(X, y)
 
-os.chdir('..')
-with open('dataset.pickle', 'wb') as f:
-    pickle.dump((X, y, dict(zip(range(0, len(instruments)), instruments))), f, pickle.HIGHEST_PROTOCOL)
+    #os.chdir('..')
+    #with open('dataset.pickle', 'wb') as f:
+    #    pickle.dump((X, y, dict(zip(range(0, len(instruments)), instruments))), f, pickle.HIGHEST_PROTOCOL)
 
-quit()
+    #quit()
 
-clsid=0
-cfile=0
-error=0
-for inst in instruments:
-    names = os.listdir(path=inst)
-    cfile += len(names)
-    for f in names:
-        samples = open_file(os.path.join(os.getcwd(), inst, f), 16000)
-        feat = mfcc(samples,16000, appendEnergy=False, winlen=0.030, VAD=simpleVAD)
-        res = gse.predict(feat[range(int(len(feat) / 2 - 10), int(len(feat) / 2 + 2))])
-        cls = int(stats.mode(res)[0])
-        if cls != clsid:
-            error += 1
-    clsid += 1
+    clsid=0
+    cfile=0
+    error=0
+    for inst in instruments:
+        names = os.listdir(path=inst)
+        cfile += len(names)
+        for f in names:
+            samples = open_file(os.path.join(os.getcwd(), inst, f), 16000)
+            feat = mfcc(samples,16000, appendEnergy=False, winlen=0.030, VAD=simpleVAD)
+            res = gse.predict(feat[range(int(len(feat) / 2 - 10), int(len(feat) / 2 + 2))])
+            cls = int(stats.mode(res)[0])
+            if cls != clsid:
+                error += 1
+        clsid += 1
 
-mse = error / cfile
-print("MSE was {0} ({1} test files)".format(mse, cfile))
+    mse = error / cfile
+    print("{0};{1}".format(set_size, mse))
 
 #print('data=[')
 #for coef in X:
